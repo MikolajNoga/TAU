af16b7c969887b1aa283303c17b8c6948909cb51
Mikolaj <nogamikolaj00@gmail.com>
2022-05-27 10:44:21 +0200

    task 5 done

--- 4/tests.py
+++ 4/tests.py
@@ -1,38 +1,102 @@
 import pytest
+import json
+
 from main import delta_function, fibonacci, gcd
 
 
+def read_file(string):
+    f = open(string + '.json')
+    data = json.load(f)
+    test_data = data['data']
+    f.close()
+    return test_data
+
+
 class TestDeltaFunction:
-    def test_negative_result_value(self):
-        assert delta_function(0.5, 2, 3) == -2
 
-    def test_positive_result_value(self):
-        assert delta_function(0.5, 5, 3) == 19
+    @classmethod
+    def setup_class(cls):
+        print('\n----setup delta tests----')
 
-    def test_non_numeric_input_should_throw_exception(self):
+    @classmethod
+    def teardown_class(cls):
+        print('\n----teardown delta tests----')
+
+    test_data = read_file('delta')
+
+    @pytest.mark.parametrize("a, b, c", [(0.5, 2, 3)])
+    def test_negative_result_value(self, a, b, c):
+        assert delta_function(a, b, c) == -2
+
+    @pytest.mark.parametrize("a, b, c", [(0.5, 5, 3)])
+    def test_positive_result_value(self, a, b, c):
+        assert delta_function(a, b, c) == 19
+
+    @pytest.mark.parametrize("a, b, c", [(0.5, 5, "t")])
+    def test_non_numeric_input_should_throw_exception(self, a, b, c):
         with pytest.raises(TypeError):
-            delta_function(0.5, 5, "t")
+            delta_function(a, b, c)
+
+    @pytest.mark.parametrize("a, b, c, expected", test_data)
+    def test_user_provided(self, a, b, c, expected):
+        assert delta_function(a, b, c) == expected
 
 
 class TestFibonacci:
-    def test_provided_negative_value(self):
+    @classmethod
+    def setup_class(cls):
+        print('\n----setup Fibonacci tests----')
+
+    @classmethod
+    def teardown_class(cls):
+        print('\n----teardown Fibonacci tests----')
+
+    test_data = read_file('fibonacci')
+
+    @pytest.mark.parametrize("n", [-4])
+    def test_provided_negative_value(self, n):
         with pytest.raises(RecursionError):
-            assert fibonacci(-4) == -2
+            assert fibonacci(n) == -2
 
-    def test_positive_result_value(self):
-        assert fibonacci(5) == 5
+    @pytest.mark.parametrize("n", [5])
+    def test_positive_result_value(self, n):
+        assert fibonacci(n) == 5
 
-    def test_non_numeric_input_should_throw_exception(self):
+    @pytest.mark.parametrize("n", ["t"])
+    def test_non_numeric_input_should_throw_exception(self, n):
         with pytest.raises(TypeError):
-            assert fibonacci('t')
+            assert fibonacci(n)
+
+    @pytest.mark.parametrize("n, expected", test_data)
+    def test_user_provided(self, n, expected):
+        assert fibonacci(n) == expected
+
 
 class TestGCD:
-    def test_provided_negative_values(self):
-        assert gcd(-4, -2) == -2
 
-    def test_provided_positive_values(self):
-        assert gcd(150, 50) == 50
+    @classmethod
+    def setup_class(cls):
+        print('\n----setup GCD tests----')
 
-    def test_non_numeric_input_should_throw_exception(self):
+    @classmethod
+    def teardown_class(cls):
+        print('\n----teardown GCD tests----')
+
+    test_data = read_file('gcd')
+
+    @pytest.mark.parametrize("a, b", [(-4, -2)])
+    def test_provided_negative_values(self, a, b):
+        assert gcd(a, b) == -2
+
+    @pytest.mark.parametrize("a, b", [(150, 50)])
+    def test_provided_positive_values(self, a, b):
+        assert gcd(a, b) == 50
+
+    @pytest.mark.parametrize("a, b", [("t", "t")])
+    def test_non_numeric_input_should_throw_exception(self, a, b):
         with pytest.raises(TypeError):
-            assert gcd("t", "t")
+            assert gcd(a, b)
+
+    @pytest.mark.parametrize("a, b, expected", test_data)
+    def test_user_provided(self, a, b, expected):
+        assert gcd(a, b) == expected
